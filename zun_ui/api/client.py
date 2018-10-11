f4a7b41ccc5b905683de8d00f662d28c508e0b40
Hongbin Lu <hongbin034@gmail.com>
2018-10-11 03:17:00 +0000

    Split command into list
    The 'command' parameter has changed from string to list [1].
    This patch updates the UI to catch this up.
    
    [1] https://review.openstack.org/#/c/580990/
    
    Change-Id: I60bdd84c22ba14e0646593a9747c5251e0dd46ff
    Partial-Bug: #1811791
    Closes-Bug: #1797285
    (cherry picked from commit 4f3be81f4cd30f73eac66924d3aaf87f5ca30bcd)

--- zun_ui/api/client.py
+++ zun_ui/api/client.py
@@ -11,6 +11,7 @@
 #    under the License.
 
 import logging
+import shlex
 
 from django.conf import settings
 
@@ -125,6 +126,8 @@ def _cleanup_params(attrs, check, **params):
                 kv = v.split("=", 1)
                 values[kv[0]] = kv[1]
             args[str(key)] = values
+        elif key == "command":
+            args[key] = shlex.split(value)
         elif key in attrs:
             if value is None:
                 value = ''
